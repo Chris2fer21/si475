4da7cd2ac7438794b5fc280fd1458bae836ca635
Administrator <administrator@kinetic.kinetic>
2021-03-23 20:15:27 -0400

    finished bump 2

--- proj3/bump.py
+++ proj3/bump.py
@@ -5,44 +5,38 @@ from turtleAPI import robot
 
 rob = robot()
 
-r = rospy.Rate(10)
+r = rospy.Rate(5)
 
 rob.drive(linSpeed=.3)
 
 while not rospy.is_shutdown():
     depth = rob.getDepth()
     rows, cols = depth.shape
-    depth = depth[(rows/2):,:]
-    #print(depth.shape)
-    left = depth[:,:(cols/3)]
-    midd = depth[:,(cols/3):(2*cols/3)]
-    righ = depth[:,(2*cols/3):]
-    #print(left.shape)
-    #print(midd.shape)
-    #print(righ.shape)
+    depth = depth[(rows/3):,:]
+    left = depth[:,(cols/5):(2*cols/5)]
+    midd = depth[:,(2*cols/5):(3*cols/5)]
+    righ = depth[:,(3*cols/5):(4*cols/5)]
+    far_l = depth[:,:(cols/5)]
+    far_r = depth[:,(4*cols/5)]
     midd = midd[np.logical_not(np.isnan(midd))]
     left = left[np.logical_not(np.isnan(left))]
     righ = righ[np.logical_not(np.isnan(righ))]
+    far_l = far_l[np.logical_not(np.isnan(far_l))]
+    far_r = far_r[np.logical_not(np.isnan(far_r))]
     midd = np.average(midd)
     righ = np.average(righ)
     left = np.average(left)
-    if midd>.5:
-        midd = .5
-    rob.drive(linSpeed=midd,angSpeed=(left-righ))
-    """
-    #print(midd.shape)
-    if midd<1:
-        #print(status)
-        rob.stop()
-        direction = .5
-        wait = 2
-        if left < righ:
-            direction = -.5
-        rob.drive(linSpeed=-.2)
-        time.sleep(1)
-        rob.drive(angSpeed=direction)
-        time.sleep(wait)
-        rob.drive(linSpeed=.3)"""
+    far_l = np.average(far_l)
+    far_r = np.average(far_r)
+    print((far_l,left,midd,righ,far_r))
+    if midd < .8:
+        midd = 0
+    turn = left-righ
+    if far_l < 1:
+        turn = -2
+    if far_r < 1:
+        turn = 2
+    rob.drive(linSpeed=.35*midd,angSpeed=2*turn)
     r.sleep()
 
 rob.stop()
